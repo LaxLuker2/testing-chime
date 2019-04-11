f9539d4a447bc612b3720d161ff296db32362263
Luke Schwarting <jeluker2@gmail.com>
2019-04-11 09:45:46 -0400

    trying something else for audio

--- server/view/main.js
+++ server/view/main.js
@@ -74,62 +74,54 @@
     audio.play();
   };
 
-  const beat1 = () => {
-    var src = $("#audio1").attr("src");
-    onSquareClicked(src);
-  };
-  const beat2 = () => {
-    var src = $("#audio2").attr("src");
-    onSquareClicked(src);
-  };
-  const beat3 = () => {
-    var src = $("#audio3").attr("src");
-    onSquareClicked(src);
-  };
-  const beat4 = () => {
-    var src = $("#audio4").attr("src");
-    onSquareClicked(src);
-  };
-  const beat5 = () => {
-    var src = $("#audio5").attr("src");
-    onSquareClicked(src);
-  };
-
-  for (let i = 0; i < colors.length; i++) {
-    if (i == 0) {
-      var x = document.createElement("audio");
-      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/knock.wav");
-      x.setAttribute("id", "audio1");
-      document.getElementById("btn1").appendChild(x);
-      colors[i].addEventListener("click", beat1);
+  window.onload = function() {
+    const beat1 = () => {
+      var src = $("#audio1").attr("src");
+      onSquareClicked(src);
+    };
+    const beat2 = () => {
+      var src = $("#audio2").attr("src");
+      onSquareClicked(src);
+    };
+    const beat3 = () => {
+      var src = $("#audio3").attr("src");
+      onSquareClicked(src);
+    };
+    const beat4 = () => {
+      var src = $("#audio4").attr("src");
+      onSquareClicked(src);
+    };
+    const beat5 = () => {
+      var src = $("#audio5").attr("src");
+      onSquareClicked(src);
+    };
+  
+    for (let i = 0; i < colors.length; i++) {
+      if (i == 0) {
+        var x = document.getElementById("audio1");
+        x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/knock.wav");
+        colors[i].addEventListener("click", beat1);
+      }
+      if (i == 1) {
+        var x = document.getElementById("audio2");
+        x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/cork.mp3");
+        colors[i].addEventListener("click", beat2);
+      }
+      if (i == 2) {
+        var x = document.getElementById("audio3");
+        x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/coins.wav");
+        colors[i].addEventListener("click", beat3);
+      }
+      if (i == 3) {
+        var x = document.getElementById("audio4");
+        x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/keys.wav");
+        colors[i].addEventListener("click", beat4);
+      }
+      if (i == 4) {
+        var x = document.getElementById("audio5");
+        x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/laugh.wav");
+        colors[i].addEventListener("click", beat5);
+      }
     }
-    if (i == 1) {
-      var x = document.createElement("audio");
-      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/cork.mp3");
-      x.setAttribute("id", "audio2");
-      document.getElementById("btn2").appendChild(x);
-      colors[i].addEventListener("click", beat2);
-    }
-    if (i == 2) {
-      var x = document.createElement("audio");
-      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/coins.wav");
-      x.setAttribute("id", "audio3");
-      document.getElementById("btn3").appendChild(x);
-      colors[i].addEventListener("click", beat3);
-    }
-    if (i == 3) {
-      var x = document.createElement("audio");
-      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/keys.wav");
-      x.setAttribute("id", "audio4");
-      document.getElementById("btn4").appendChild(x);
-      colors[i].addEventListener("click", beat4);
-    }
-    if (i == 4) {
-      var x = document.createElement("audio");
-      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/laugh.wav");
-      x.setAttribute("id", "audio5");
-      document.getElementById("btn5").appendChild(x);
-      colors[i].addEventListener("click", beat5);
-    }
-  }
+  };
 })();
