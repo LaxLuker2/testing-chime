b670e81043385be4c9734d2f6ef5b9eab40cf848
Luke Schwarting <jeluker2@gmail.com>
2019-04-10 23:53:36 -0400

    took out old code

--- server/view/main.js
+++ server/view/main.js
@@ -74,20 +74,6 @@
     audio.play();
   };
 
-  // const songClick = count => {
-  //   if (count == 1) {
-  //     onSquareClicked("https://mysound.cad.rit.edu/audioFiles/knock.wav");
-  //   } else if (count == 2) {
-  //     onSquareClicked("https://mysound.cad.rit.edu/audioFiles/cork.mp3");
-  //   } else if (count == 3) {
-  //     onSquareClicked("https://mysound.cad.rit.edu/audioFiles/coins.wav");
-  //   } else if (count == 4) {
-  //     onSquareClicked("https://mysound.cad.rit.edu/audioFiles/keys.wav");
-  //   } else if (count == 5) {
-  //     onSquareClicked("https://mysound.cad.rit.edu/audioFiles/laugh.wav");
-  //   }
-  // };
-
   const beat1 = () => {
     var src = $("#audio1").attr("src");
     onSquareClicked(src);
@@ -108,12 +94,6 @@
     var src = $("#audio5").attr("src");
     onSquareClicked(src);
   };
-
-  // document.getElementById("btn1").addEventListener("click", beat1);
-  // document.getElementById("btn2").addEventListener("click", beat2);
-  // document.getElementById("btn3").addEventListener("click", beat3);
-  // document.getElementById("btn4").addEventListener("click", beat4);
-  // document.getElementById("btn5").addEventListener("click", beat5);
 
   for (let i = 0; i < colors.length; i++) {
     if (i == 0) {
@@ -152,11 +132,4 @@
       colors[i].addEventListener("click", beat5);
     }
   }
-
-  $("#btn1").med;
-
-  // for (let i = 0; i < colors.length; i++) {
-  //   console.log("in for");
-  //   colors[i].addEventListener("click", songClick);
-  // }
 })();
