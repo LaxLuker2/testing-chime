0c638c61468569d920b69a6c2f93180c88603e90
Luke Schwarting <jeluker2@gmail.com>
2019-04-11 10:16:59 -0400

    Trying new client.js audio and pulling from people.rit.edu

---
+++ server/view/client.js
@@ -0,0 +1,58 @@
+const colors = document.getElementsByClassName("color");
+const onSquareClicked = song => {
+    console.log("colors clicked");
+    console.log(song);
+    var audio = new Audio(song);
+    audio.play();
+  };
+
+  window.onload = function() {
+    const beat1 = () => {
+      var src = $("#audio1").attr("src");
+      onSquareClicked(src);
+    };
+    const beat2 = () => {
+      var src = $("#audio2").attr("src");
+      onSquareClicked(src);
+    };
+    const beat3 = () => {
+      var src = $("#audio3").attr("src");
+      onSquareClicked(src);
+    };
+    const beat4 = () => {
+      var src = $("#audio4").attr("src");
+      onSquareClicked(src);
+    };
+    const beat5 = () => {
+      var src = $("#audio5").attr("src");
+      onSquareClicked(src);
+    };
+  
+    for (let i = 0; i < colors.length; i++) {
+      if (i == 0) {
+        var x = document.getElementById("audio1");
+        x.setAttribute("src", "https://people.rit.edu/lgs9654/audioFiles/knock.wav");
+        colors[i].addEventListener("click", beat1);
+      }
+      if (i == 1) {
+        var x = document.getElementById("audio2");
+        x.setAttribute("src", "https://people.rit.edu/lgs9654/audioFiles/cork.mp3");
+        colors[i].addEventListener("click", beat2);
+      }
+      if (i == 2) {
+        var x = document.getElementById("audio3");
+        x.setAttribute("src", "https://people.rit.edu/lgs9654/audioFiles/coins.wav");
+        colors[i].addEventListener("click", beat3);
+      }
+      if (i == 3) {
+        var x = document.getElementById("audio4");
+        x.setAttribute("src", "https://people.rit.edu/lgs9654/audioFiles/keys.wav");
+        colors[i].addEventListener("click", beat4);
+      }
+      if (i == 4) {
+        var x = document.getElementById("audio5");
+        x.setAttribute("src", "https://people.rit.edu/lgs9654/audioFiles/laugh.wav");
+        colors[i].addEventListener("click", beat5);
+      }
+    }
+  };
